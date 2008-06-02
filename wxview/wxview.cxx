c92e0f5866f6353337405efdaa48d2a7946c40d2
Vasily i. Redkin <vir@ctm.ru>
2008-06-02 17:54:44 +0000

    Command line parsing added.

--- wxview/wxview.cxx
+++ wxview/wxview.cxx
@@ -1,12 +1,18 @@
 #include <wx/wx.h>
+#include <wx/cmdline.h>
 #include "MyFrame.hpp"
 #include "MyDocument.hpp"
  
 class MyApp : public wxApp
 {
+	private:
+		std::string fname;
+		unsigned int startpage;
 	public:
 		virtual bool OnInit();
 		virtual int OnExit() { return 0; }
+		virtual void OnInitCmdLine(wxCmdLineParser& parser);
+		virtual bool OnCmdLineParsed(wxCmdLineParser& parser);
 };
 
 // Create a new application object: this macro will allow wxWidgets to create
@@ -19,12 +25,50 @@ IMPLEMENT_APP(MyApp)
 // `Main program' equivalent: the program execution "starts" here
 bool MyApp::OnInit()
 {
+	startpage = 1;
+	if(!wxApp::OnInit()) // it parses command line
+		return false;
 	theDocument = new MyDocument();
-	if(! theDocument->LoadFile("/home/vir/in-sync/TP_VR_2008.pdf")) return false;
-	if(! theDocument->LoadPage(40)) return false;
+	if(! theDocument->LoadFile(fname)) return false;
+	if(! theDocument->LoadPage(startpage)) return false;
 	MyFrame *frame = new MyFrame(_T("My Buggy PDF Viewer"), wxPoint(10, 10), wxSize(1100, 1000));
 	frame->Show(true);
 	SetTopWindow(frame);
 	return true;
 }
+
+static const wxCmdLineEntryDesc g_cmdLineDesc [] =
+{
+	{ wxCMD_LINE_SWITCH, wxT("h"), wxT("help"), wxT("displays help on the command line parameters"), wxCMD_LINE_VAL_NONE, wxCMD_LINE_OPTION_HELP },
+	{ wxCMD_LINE_OPTION, wxT("p"), wxT("page"), wxT("start page"), wxCMD_LINE_VAL_NUMBER, wxCMD_LINE_PARAM_OPTIONAL },
+//	{ wxCMD_LINE_SWITCH, wxT("t"), wxT("test"), wxT("test switch"), wxCMD_LINE_VAL_NONE, wxCMD_LINE_OPTION_MANDATORY  },
+//	{ wxCMD_LINE_SWITCH, wxT("s"), wxT("silent"), wxT("disables the GUI") }, 
+	{ wxCMD_LINE_PARAM,  NULL, NULL, wxT("input file"), wxCMD_LINE_VAL_STRING, /*wxCMD_LINE_PARAM_MULTIPLE*/wxCMD_LINE_OPTION_MANDATORY },
+	{ wxCMD_LINE_NONE }
+};
+
+void MyApp::OnInitCmdLine(wxCmdLineParser& parser)
+{
+	parser.SetDesc(g_cmdLineDesc);
+	parser.SetSwitchChars(wxT("-"));
+}
+
+
+bool MyApp::OnCmdLineParsed(wxCmdLineParser& parser)
+{
+#if 0
+	silent_mode = parser.Found(wxT("s"));
+	wxArrayString files;
+	for (int i = 0; i < parser.GetParamCount(); i++) { files.Add(parser.GetParam(i)); }
+#endif
+	parser.Found(wxT("p"), (long int *)&startpage);
+std::cerr << "Params count: " << parser.GetParamCount() << std::endl;
+	if(parser.GetParamCount() == 1) {
+		fname = parser.GetParam(0).mb_str();
+	} else {
+		return false;
+	}
+	return true;
+}
+
 
