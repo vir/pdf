bd7d3e7622a5d693dde8cb21c7ad81cd91279730
Vasily i. Redkin <vir@ctm.ru>
2008-06-06 13:26:58 +0000

    namespace fix for msvc

--- libpdf/Filter_Base85.cxx
+++ libpdf/Filter_Base85.cxx
@@ -68,7 +68,7 @@ bool Base85Filter::Decode(const std::vector<char> & src, std::vector<char> & dst
 	b85dh h;
 	dst.reserve(src.size()*4/5);
 	while(1) {
-		while(std::strchr(Whitespace,src[pos]) && pos<src.size()) pos++;
+		while(strchr(Whitespace,src[pos]) && pos<src.size()) pos++;
 		if(src[pos] == '~' && src[pos + 1] == '>') {
 			h.flush(dst);
 			break;
