e1b1664c048dba152d1a0514bf31bdd08632043d
Vasily i. Redkin <vir@ctm.ru>
2008-06-07 09:27:27 +0000

    All-in-one header file

---
+++ libpdf/PDF.hpp
@@ -0,0 +1,11 @@
+
+#include "libpdf/File.hpp"
+#include "libpdf/Document.hpp"
+#include "libpdf/Page.hpp"
+#include "libpdf/Font.hpp"
+#include "libpdf/OH.hpp"
+#include "libpdf/Exceptions.hpp"
+#include "libpdf/Point.hpp"
+#include "libpdf/Media.hpp"
+#include "libpdf/ObjectsCache.hpp"
+
