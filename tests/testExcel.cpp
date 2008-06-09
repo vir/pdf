03b69c18512a7b4b992b40c172a2a0c844d23cf1
Vasily i. Redkin <vir@ctm.ru>
2008-06-09 03:34:32 +0000

    Excel wrapper test project

---
+++ tests/testExcel.cpp
@@ -0,0 +1,29 @@
+// testExcel.cpp : Defines the entry point for the application.
+//
+
+#include "../tabulator/Excel.hpp"
+
+using namespace std;
+
+int APIENTRY WinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPSTR     lpCmdLine,
+                     int       nCmdShow)
+{
+	Excel e;
+	if(	e.open() ) {
+		//TRACE("Open successed");
+	} else {
+		//TRACE("Open failed");
+		e.create();
+		e.add_workbook();
+	}
+	e.set_visible(true);
+	e.move_cursor(2, 1);
+	e.set_cell_value(L"Hello!");
+	e.move_cursor(-1, 0);
+	return 0;
+}
+
+
+
