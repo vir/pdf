4488939ed2fd751e5cd51ebd349c27e281f5fdb3
Vasily i. Redkin <vir@ctm.ru>
2008-06-23 04:08:30 +0000

    Rolled back last change - have to be more clever comparing textblock coordinates

--- tabulator/Tabulator_TextBlock.cxx
+++ tabulator/Tabulator_TextBlock.cxx
@@ -4,12 +4,12 @@ PDF::Rect Tabulator::TextBlock::bounds() const
 {
 	double w = width; // XXX
 	double h = height; // XXX \todo write it!
-	return PDF::Rect(x, y, w, h);
+	return PDF::Rect(pos.x, pos.y, w, h);
 }
 
 bool Tabulator::TextBlock::merge_ok(const TextBlock & oth) const
 {
-	std::clog << "merge_ok(" << x << ',' << y << ")(" << oth.x << ',' << oth.y << "): ";
+	std::clog << "merge_ok" << pos.dump() << oth.pos.dump() << ": ";
 	if(Tabulator::Coord::compare(angle, oth.angle)) { // different angles
 		std::clog << "Different angles" << std::endl;
 		return false;
@@ -18,12 +18,12 @@ bool Tabulator::TextBlock::merge_ok(const TextBlock & oth) const
 		std::cerr << "Only horizontal text joins currently" << std::endl;
 		return false;
 	}
-	if(Tabulator::Coord::compare(y, oth.y)) { // Not on one line
+	if(Tabulator::Coord::compare(pos.y, oth.pos.y)) { // Not on one line
 		std::clog << "Not on one line" << std::endl;
 		return false;
 	}
 	double avgcw = width / double(text.length());
-	double distance = oth.x - (x + width);
+	double distance = oth.pos.x - (pos.x + width);
 	bool result = distance < 0.25 * height;
 	std::clog << "Average charwidth: " << avgcw << ", distance: " << distance << ", result: " << result << std::endl;
 //	return (abs(distance) < 2*avgcw);
@@ -33,7 +33,7 @@ bool Tabulator::TextBlock::merge_ok(const TextBlock & oth) const
 Tabulator::TextBlock Tabulator::TextBlock::operator +(const Tabulator::TextBlock & oth) const
 {
 	std::wstring newtext = text;
-	double distance = oth.x - (x + width);
+	double distance = oth.pos.x - (pos.x + width);
 #if 0
 	double font_size = height;
 	if(distance > 0.15*font_size) {
@@ -46,7 +46,7 @@ Tabulator::TextBlock Tabulator::TextBlock::operator +(const Tabulator::TextBlock
 	double newheight = height;
 	if(oth.height > height)
 		newheight = oth.height;
-	return Tabulator::TextBlock(x, y, angle, newtext, newwidth, newheight);
+	return Tabulator::TextBlock(pos, angle, newtext, newwidth, newheight);
 }
 
 
