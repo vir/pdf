863aafb30c628db99a73a27d2566afea810e1b23
Vasily i. Redkin <vir@ctm.ru>
2008-06-25 13:14:37 +0000

    Standard PDF fonts support

---
+++ libpdf/Font_StdFonts.cxx
@@ -0,0 +1,30 @@
+#include "Font_StdFonts.hpp"
+#include <iostream>
+/*
+ * Font metrics available from http://www.adobe.com/devnet/font/
+ * (link at page bottom).
+ */
+
+struct width_table {
+	const char * name;
+	const short * w;
+	unsigned int wn;
+};
+
+#include "deffont_widths.i"
+
+void load_stdfont_widths_table(std::map<int, unsigned long> & charwidths, std::string basefont)
+{
+	unsigned int i, c;
+	for(i = 0; i < sizeof(standard_font_widths_table)/sizeof(standard_font_widths_table[0]); i++) {
+		if(basefont == standard_font_widths_table[i].name) {
+			for(c = 0; c < standard_font_widths_table[i].wn; c++) {
+				charwidths[c] = standard_font_widths_table[i].w[c];
+			}
+			return;
+		}
+	} // for all fonts
+	std::cerr << "Can not found widths array for standard font " << basefont << std::cerr;
+}
+
+
---
+++ libpdf/Font_StdFonts.hpp
@@ -0,0 +1,9 @@
+#ifndef STDFONT_METRICS_H_INCLUDED
+#define STDFONT_METRICS_H_INCLUDED
+#include <map>
+#include <string>
+
+void load_stdfont_widths_table(std::map<int, unsigned long> & charwidths, std::string basefont);
+
+#endif /* STDFONT_METRICS_H_INCLUDED */
+
