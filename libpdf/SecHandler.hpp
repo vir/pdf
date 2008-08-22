f9a98b784531ac0e73e2a3f3200e37694b28a22a
Vasily i. Redkin <vir@ctm.ru>
2008-08-22 11:52:23 +0000

    Security handler interface

---
+++ libpdf/SecHandler.hpp
@@ -0,0 +1,22 @@
+#ifndef SECHANDLER_HPP_INCLUDED
+#define SECHANDLER_HPP_INCLUDED
+
+#include <string>
+
+namespace PDF {
+
+class Dictionary;
+class File;
+
+class SecHandler
+{
+	public:
+		static SecHandler * create(const Dictionary * cryptodict, const File * file = NULL);
+		virtual bool set_password(const std::string & pw, int which = 0) = 0;
+		virtual void decrypt_object(long num, long gen, char * buf, long len) = 0;
+};
+
+} // namespace PDF
+
+#endif /* SECHANDLER_HPP_INCLUDED */
+
