865340ffc8638d7f1d95729f6ef0084b014070ce
Vasily i. Redkin <vir@ctm.ru>
2011-12-28 13:00:10 +0000

    Added new objects creation. Some renames.

--- libpdf/ObjectsCache.cxx
+++ libpdf/ObjectsCache.cxx
@@ -14,9 +14,12 @@ OH ObjectsCache::get_object(const ObjId & id)
   CacheIterator it=cache.find(id);
   if(it == cache.end()) // load
   {
-    Object * o=file.load_object(id); if(!o) throw std::string("Can't load object");
+    Object * o=m_file.load_object(id);
+    if(!o)
+      throw std::exception("Can't load object from file");
     std::pair<CacheIterator, bool> r=cache.insert(std::make_pair<ObjId, CacheEntry>(id, CacheEntry(o)));
-    if(!r.second) throw std::string("Can't insert object into cache");
+    if(!r.second)
+      throw std::exception("Can't insert object into cache");
     return OH(this, o);
   }
   else // here
@@ -31,6 +34,27 @@ OH ObjectsCache::get_handle(Object * o)
 		return get_object(o->m_id);
 	} else {
 		return OH(this, o);
+	}
+}
+
+PDF::OH ObjectsCache::new_object( Object * o )
+{
+	if(o->indirect) {
+		std::pair<CacheIterator, bool> r = cache.insert(std::make_pair<ObjId, CacheEntry>(o->m_id, CacheEntry(o)));
+		if(!r.second)
+			throw std::exception("Can't insert object into cache");
+		return OH(this, o);
+	} else
+		return get_handle(o);
+}
+
+void ObjectsCache::save_modified()
+{
+	for(CacheIterator it = cache.begin(); it != cache.end(); ++it) {
+		if(it->second.is_modified) {
+			m_file.save_object(it->second.obj);
+			it->second.is_modified = false;
+		}
 	}
 }
 
--- libpdf/ObjectsCache.hpp
+++ libpdf/ObjectsCache.hpp
@@ -26,11 +26,11 @@ class ObjectsCache
     {
       private:
       public:
-        CacheEntry(const CacheEntry & x):obj(x.obj),usecount(x.usecount)
+        CacheEntry(const CacheEntry & x):obj(x.obj),usecount(x.usecount),is_modified(x.is_modified)
         {
 //          std::clog << "CopyConstructing CacheEntry" << this << "(" << &x << ")" << std::endl;
         }
-        CacheEntry(Object * o=NULL, unsigned int c=0):obj(o),usecount(c)
+        CacheEntry(Object * o=NULL, unsigned int c=0, bool modified=false):obj(o),usecount(c),is_modified(modified)
         {
 //          std::clog << "Constructing CacheEntry" << this << "(" << o->dump() << ")" << std::endl;
         }
@@ -38,10 +38,12 @@ class ObjectsCache
         {
 //          std::clog << "Deleting CacheEntry" << this << std::endl;
         }
+        void set_modified() { is_modified = true; }
         Object * obj;
         unsigned long usecount;
+        bool is_modified;
     };
-    File & file;
+    File & m_file;
     std::map<ObjId, CacheEntry> cache;
     typedef std::map<ObjId, CacheEntry>::iterator CacheIterator;
   protected:
@@ -51,8 +53,9 @@ class ObjectsCache
     }
     void dec_counter(CacheIterator it)
     {
-      it->second.usecount--;
-      if(it->second.usecount == 0)
+      if(it->second.usecount)
+        it->second.usecount--;
+      if(it->second.usecount == 0 && !it->second.is_modified)
       {
         delete it->second.obj;
         cache.erase(it);
@@ -61,28 +64,31 @@ class ObjectsCache
     void inc_counter(const ObjId & id)
     {
       CacheIterator it=cache.find(id);
-      if(it==cache.end()) throw std::string("Object not in cache");
+      if(it==cache.end()) throw std::exception("Object not in cache");
       else inc_counter(it);
     }
     void dec_counter(const ObjId & id)
     {
       CacheIterator it=cache.find(id);
-      if(it==cache.end()) throw std::string("Object not in cache");
+      if(it==cache.end()) throw std::exception("Object not in cache");
       else dec_counter(it);
     }
   public:
-    ObjectsCache(File & f):file(f)  { }
+    ObjectsCache(File & f):m_file(f)  { }
     ~ObjectsCache()
     {
       // delete all cached objects
       for(CacheIterator it=cache.begin(); it!=cache.end(); it++) delete it->second.obj;
     }
-    OH get_object(const ObjId & id);
-	OH get_handle(Object * o);
+		OH get_object(const ObjId & id);
+		OH get_handle(Object * o);
 		void flush()
 		{
 			cache.clear();
 		}
+		OH new_object(Object * o);
+		void save_modified();
+		File & file() { return m_file; }
 };
 
 
