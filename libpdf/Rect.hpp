90c8355d5ec5865ce7671451e86a8f201ef70e1a
Vasily i. Redkin <vir@ctm.ru>
2011-12-28 13:06:21 +0000

    Added one more constructor (fro Point, width and height). Added clip_him(...) method.

--- libpdf/Rect.hpp
+++ libpdf/Rect.hpp
@@ -20,11 +20,25 @@ class Rect
     Rect(double a[4]):x1(a[0]),y1(a[1]),x2(a[2]),y2(a[3]) { normalize(); }
     Rect(double nx1, double ny1, double nx2, double ny2):x1(nx1),y1(ny1),x2(nx2),y2(ny2) { normalize(); }
     Rect(const Point & p1, const Point & p2):x1(p1.x),y1(p1.y),x2(p2.x),y2(p2.y) { normalize(); }
+    Rect(const Point & p1, double w, double h):x1(p1.x),y1(p1.y),x2(p1.x + w),y2(p1.y + h) { normalize(); }
     Rect & operator =(const Rect & o) { x1=o.x1; y1=o.y1; x2=o.x2; y2=o.y2; return *this; }
     std::string dump() const { std::stringstream ss; ss << "(" << x1 << "," << y1 << "," << x2 << "," << y2 << ")"; return ss.str(); }
 		Point offset() const { return Point(x1,y1); }
 		Point size() const { return Point(x2-x1, y2-y1); }
 		bool in(Point p) const { return p.x>=x1 && p.x<=x2 && p.y>=y1 && p.y<=y2; }
+		bool normalized() const { return x2 >= x1 && y2 >= y1; }
+		bool clip_him(Rect& oth) const
+		{
+			bool r = true;
+			using namespace std;
+			oth.x1 = max(oth.x1, x1);
+			oth.x2 = min(oth.x2, x2);
+			oth.y1 = max(oth.y1, y1);
+			oth.y2 = min(oth.y2, y2);
+			if(oth.x2 < oth.x1) { oth.x2 = oth.x1; r = false; }
+			if(oth.y2 < oth.y1) { oth.y2 = oth.y1; r = false; }
+			return r;
+		}
 //    bool operator < (const Point & p) const { return (y == p.y)?x<p.x:y<p.y; }
 };
   
